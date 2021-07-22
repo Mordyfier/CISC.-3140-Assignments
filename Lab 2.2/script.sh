a1b6616ad44b9cbc793f3a459acf422075cc8b89
Martin T <mordyyy@outlook.com>
2021-07-22 15:26:40 -0400

    Minor tweaks in script.sh, mostly comments

--- Lab 2.2/script.sh
+++ Lab 2.2/script.sh
@@ -5,16 +5,20 @@ echo "year, movie_count, action_count, percentage_of_all" > actiondata.csv
 # The script iterates through all the years in the range to produce the report for each year
 
 for i in {1948..2018}
-do
+do	# grep filters current year's movies and sends them all to a text file
 	grep "($i)" movies.csv > currentyear.txt
+	# awk command to obtain the number of lines in the file - in this case, number of current year's movies
 	totalcount=`awk 'END { print NR }' currentyear.txt`
+	# awk filter - only lines that include 'Action' will be counted
 	awk '/Action/ {print}' currentyear.txt > action.txt
+	# number of action movies
 	count=`awk 'END { print NR }' action.txt`
 	# Division by 0 protection
 	if [ $count == "0" ]
 	then
 		actionperc=0
-	else
+	else	
+		# percentage of Action movies in a given year
 		actionperc=`echo "$count $totalcount" | awk '{printf "%.1f\n", ($1/$2)*100}'`
 	fi
 	echo "The year is $i, all movies: $totalcount, number of action movies: $count, percentage: $actionperc%"
